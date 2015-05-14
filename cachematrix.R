891f47be7b66d2960cff659a030cd45e051612b9
ROBERT WINFIELD <trebor_at_brum@hotmail.com>
2015-05-14 23:54:32 +0100

    ammended some comments

--- cachematrix.R
+++ cachematrix.R
@@ -13,14 +13,17 @@
 
 ##      -Two functions to demonstrate lexical scoping in R-
 
-## makeCacheMatix returns a named list to implement caching
-## This named list contains functions to access and set both the
+## 1/                  makeCacheMatix(x) 
+
+## Returns a named list to implement caching
+## This list exposes functions to both set and access the
 ## target matrix and its inverse.
 
+## 2/                   cacheSolve(x,...)
+
 ## cacheSolve(x,...) inspects the global environment to see if the inverse
-## either needs to be computed or can simply be cached. If the computation is 
-## computationally expensive then caching of previous results makes sense.
-## This will frequently be the case for large matrices
+## can simply be cached. Otherwise inverse is computed. 
+
 
 ## usage: closure<-makeCacheMatrix(yourSquareMatrix)
 ##        cacheSolve(closure) first call will compute inverse and cache it.
@@ -28,18 +31,20 @@
 
 ## ------------------------------------------------------------------
 
-## Function makeCacheMatrix
+## Function makeCacheMatrix(x)
  
-## This function returns a named list referring to four internally defined functions which
-## in effect are returned as parameters. (functions are first class citizen in R)
+## This function returns a named list referring to four 
+## internally defined functions which in effect
+## are returned as parameters. (functions are first class citizen in R)
 
 makeCacheMatrix <- function(x = matrix()) {
   
   mtx_cached<-NULL  ## initialize cached matrix variable to null
   
-  set<-function(y){ ## assigns a new matrix y to x, in the global environment for subsequent inversion
-                    ## This function will be called via the return named list and so x will be taken from the
-                    ## global environment due to lexical scoping.
+  set<-function(y){ ## assigns a new matrix y to x, in the global environment 
+    ## for subsequent inversion. This function will be called via the return
+    ## named list and so x will be taken from theglobal environment due to 
+    ## lexical scoping.
     x<<-y
     mtx_cached<-NULL ## re-initialize to null
   }
@@ -47,7 +52,9 @@ makeCacheMatrix <- function(x = matrix()) {
   get<-function()x
   
   ## Note that <<- operator assigns inverse to mtx_cached and stores it in
-  ## the global environment. Any subsequent call will retrieve the cached inverse matrix
+  ## the global environment. Any subsequent call will retrieve the cached 
+  ## inverse matrix
+  
   setinverse<-function(inverse) mtx_cached<<-inverse
   
   getinverse<-function() mtx_cached  ## simply retrieves cached inverse matrix
@@ -66,12 +73,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Take named list argument x. If inverse already cached then return that
 ## otherwise calculate its inverse and store it back in the global environment
 
-## Argument x takes a named list populated with functions from makeCacheMatrix (see usage above)
+## Argument x takes a named list populated with functions from makeCacheMatrix 
+## (see usage above)
 
-cacheSolve <- function(x, ...) { ## Argument x assigned to a named list returned by a call to makeCacheMatrix
-                                 ## for inversion
-  
-  ## Return cached matrix inverse or compute it otherwise
+
+cacheSolve <- function(x, ...) { 
+                                 
+  ## Argument x assigned to a named list returned by a call to makeCacheMatrix
+  ## for inversion
   
   invmtx<-x$getinverse() ## assign inverse matrix(maybe null) to invmtx
   
@@ -80,13 +89,13 @@ cacheSolve <- function(x, ...) { ## Argument x assigned to a named list returned
     return(invmtx)
   }
   
-  ## inverse has not been calculated yet,we do that now
+  ## otherwise calculate and cache the inverse
   
-  rawMatrix<-x$get() ## access get function from named list x to retrieve raw matrix for inversion
+  rawMatrix<-x$get()  ## retrieve raw matrix for inversion
   
   j<-solve(rawMatrix) ## compute inverse of the matrix
   
-  x$setinverse(j) ## access setinverse function and cache the inverse
+  x$setinverse(j)     ## cache the inverse
   
   j ## report the inverted matrix
 }
